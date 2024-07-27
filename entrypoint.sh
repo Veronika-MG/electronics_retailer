b0242279ff8b8ee9153b5aeeec3a6f7ccfe9b1c3
Veronika <veronika-mg@bk.ru>
2024-07-27 14:58:56 +0300

    update

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-python electronics_retailer/manage.py migrate
-if python electronics_retailer/manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.exists())" | grep -q "False"; then
-    python electronics_retailer/manage.py loaddata electronics_retailer/fixtures/data.json
-fi
-exec "$@"
